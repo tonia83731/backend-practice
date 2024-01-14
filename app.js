c7ec30567cc366fa4761fa25b6f8493dc84b8fc7
tonia94731 <huiyu.tonia.chang@gmail.com>
2024-01-14 21:41:05 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -56,6 +56,13 @@ app.post('/todos', (req, res) => {
     })
     .catch((error) => console.log(error));
 })
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render("detail", { todo }))
+    .catch((error) => console.log(error));
+});
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`);
