bf25b46cb10a320ecaffd516f952b87e7d00b966
tonia94731 <huiyu.tonia.chang@gmail.com>
2024-01-26 16:38:20 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,5 +4,9 @@ const router = express.Router();
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.post('/login', (req, res) => {})
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 export default router
