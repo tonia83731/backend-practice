3d7ca8fa5d64dde53cc8ec29914370e45ff743db
tonia94731 <huiyu.tonia.chang@gmail.com>
2024-02-05 22:18:05 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,30 +1,48 @@
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
 import Todo from "../todo.js";
+import User from "../user.js";
 import db from "../../config/mongoose.js";
 
-// if (process.env.NODE_ENV !== "production") {
-//   dotenv.config();
-// }
+if (process.env.NODE_ENV !== "production") {
+  // console.log("here");
+  dotenv.config();
+}
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const SEED_USER = {
+  name: "root",
+  email: "root@example.com",
+  password: "12345678",
+};
 
-// const db = mongoose.connection;
-// db.on("error", (err) => {
-//   console.error("mongodb error!", err);
-// });
 db.once("open", () => {
-  console.log("mongodb connected!");
-  for (let i = 0; i < 10; i++) {
-    Todo.create({
-      name: `My todo name-${i}`,
-      type: "work",
-      description: `This is todolist item No.${i}`,
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      for (let i = 0; i < 10; i++) {
+        return Promise.all(
+          Array.from({ length: 10 }, (_, i) =>
+            Todo.create({
+              name: `My todo name-${i}`,
+              type: "work",
+              description: "This is todo description",
+              userId,
+            })
+          )
+        );
+      }
+    })
+    .then(() => {
+      console.log("done.");
+      process.exit();
     });
-  }
-  console.log('done')
 });
